d5e9c093add3b1231d0dcf3c9549274a9ca7c5dd
GL <375806937@qq.com>
2018-12-24 15:56:49 +0800

    web

---
+++ web/index.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,Markup
+
+app =Flask(__name__, static_folder='static', template_folder='template')
+
+@app.template_global()
+def hello():
+    print("hello world")
+    return 'hello world'
+
+@app.route('/')
+def index():
+    return render_template('index.html', date = ['2321321','sadsadsad','dsaiufhsakfsa'])
+
+
+@app.context_processor
+def show():
+    f = "hello world"
+    return dict(f = f)
+
+@app.template_filter()
+def my_s(s):
+    return str(s) + Markup(' &#9835;')
+
+@app.template_test()
+def enmptry(s):
+    if s:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', port= 9000, debug= True)
